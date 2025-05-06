57f0919829d5dc8712f81db9ba4df9e948eb0916
Juan Rempel <juanrempel93@gmail.com>
2025-05-05 21:51:04 -0500

    added markdown config

---
+++ after/ftplugin/markdown.lua
@@ -0,0 +1,7 @@
+-- ~/.config/nvim/after/ftplugin/markdown.lua
+vim.schedule(function()
+  vim.opt_local.shiftwidth = 2
+  vim.opt_local.tabstop = 2
+  vim.opt_local.softtabstop = 2
+  vim.opt_local.expandtab = true
+end)
